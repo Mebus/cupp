188dd6e02ffaacf5be648adcab9747fae177fa3b
Abhro Rahman <rabhro@gmail.com>
2016-06-24 14:51:30 +0600

    Added python 3 port

---
+++ test_cupp.py
@@ -0,0 +1,16 @@
+
+import unittest
+from cupp3 import *
+
+class TestCupp3(unittest.TestCase):
+    def setUp(self):
+        read_config()
+
+    def test_ftp_download(self):
+        pass
+
+    def test_parser(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
