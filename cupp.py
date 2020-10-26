2b0b7ea777c4fde9f71130972ff15bff01cebe40
quantumcore <fastcorp11@gmail.com>
2020-10-26 23:27:24 +0500

    fixed quitting error

--- cupp.py
+++ cupp.py
@@ -118,6 +118,13 @@ def komb(seq, start, special=""):
 
 
 def print_to_file(filename, unique_list_finished):
+
+    def clear_screen():
+        if(os.name == "nt"):
+            os.system("cls")
+        else:
+            os.system("clear")
+
     f = open(filename, "w")
     unique_list_finished.sort()
     f.write(os.linesep.join(unique_list_finished))
@@ -134,15 +141,18 @@ def print_to_file(filename, unique_list_finished):
         + str(lines)
         + " words.\033[1;m"
     )
-    inspect = input("> Hyperspeed Print? (Y/n) : ").lower()
+    inspect = input("[+] Hyperspeed Print? (Y/n) : ").lower()
     if inspect == "y":
         try:
             with open(filename, "r+") as wlist:
                 data = wlist.readlines()
                 for line in data:
-                    print("\033[1;32m[" + filename + "] \033[1;33m" + line)
-                    time.sleep(0000.1)
-                    os.system("clear")
+                    try:
+                        print("\033[1;32m[" + filename + "] \033[1;33m" + line)
+                        time.sleep(0000.1)
+                        clear_screen()
+                    except KeyboardInterrupt:
+                        break
         except Exception as e:
             print("[ERROR]: " + str(e))
     else:
